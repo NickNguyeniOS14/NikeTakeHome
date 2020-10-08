542b199ab25c5ceca84d9b4800ea69fb8d01030e
Nick <ptnguyen1901@gmail.com>
2020-10-08 14:03:58 -0400

    setup model

---
+++ NikeTakeHome/Model Controller/ModelController.swift
@@ -0,0 +1,8 @@
+//
+//  ModelController.swift
+//  NikeTakeHome
+//
+//  Created by Nick Nguyen on 10/8/20.
+//
+
+import Foundation
---
+++ NikeTakeHome/Model/Album.swift
@@ -0,0 +1,34 @@
+//
+//  Model.swift
+//  NikeTakeHome
+//
+//  Created by Nick Nguyen on 10/8/20.
+//
+
+import Foundation
+// Cell : shows name of album, artist, thumbnail image
+// Detail: (bonus) larger image,  genre, release date, copyright info, a button to show website
+
+struct Album: Decodable {
+    let artistName: String
+    let id: String
+    let releaseDate: String
+    let name: String
+    let copyright: String
+    let artistUrl: String
+    let url: String
+    let genres: [Genre]
+}
+
+struct Genre: Decodable {
+    let name: String
+}
+
+struct Feed: Decodable {
+    let results: [Album]
+}
+
+// Result
+struct RootJSON: Decodable {
+    let feed: Feed
+}
