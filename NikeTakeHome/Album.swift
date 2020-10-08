47559f5ba34429deb3d0dc7f18fc02a36f6f0ca8
Nick <ptnguyen1901@gmail.com>
2020-10-08 13:32:07 -0400

    remove storyboard

---
+++ NikeTakeHome/Album.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  NikeTakeHome
+//
+//  Created by Nick Nguyen on 10/8/20.
+//
+
+import Foundation
+
+struct Album: Codable {
+    let artistName: String
+    let id: String
+    let releaseDate: String
+    
+}
---
+++ NikeTakeHome/NetworkService.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkService.swift
+//  NikeTakeHome
+//
+//  Created by Nick Nguyen on 10/8/20.
+//
+
+import Foundation
+
+//https://rss.itunes.apple.com/api/v1/us/apple-music/coming-soon/all/100/explicit.json
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
